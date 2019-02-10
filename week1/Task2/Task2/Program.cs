6f364f106be0672f1d9cacdc96cf3cf10ab13b4e
Aruzhan Karazhanova <aruka010900@gmail.com>
2019-02-10 23:43:51 +0600

    task2 added

---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string ID;
+        public int year = 1;
+
+        public Student (string name, string ID)
+        {
+            this.name = name;
+            this.ID = ID;
+        }
+        public void print()
+        {
+            year++;
+            Console.WriteLine(name + " " + ID + " " + year);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student info = new Student("Aruzhan", "18BD110118");
+            info.print();
+        }
+    }
+}
