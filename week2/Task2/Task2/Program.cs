54ecaa72f732c05fe60655c1f044eeceee542472
Aruzhan Karazhanova <aruka010900@gmail.com>
2019-02-14 11:32:06 +0600

    some changes

--- week2/Task2/Task2/Program.cs
+++ week2/Task2/Task2/Program.cs
@@ -15,10 +15,7 @@ namespace Task2
 
             bool result = true;
 
-            if (a == 1)
-            {
-                result = false;
-            }
+            if (a == 1) result = false;
 
             for (int i = 2; i < a; i++)
             {
@@ -30,30 +27,48 @@ namespace Task2
             return result;
         }
 
-    
-        static void Primes()
+
+        static bool PrimeString(string s)
         {
-           StreamReader input = new StreamReader(@"C: \Users\Аружан\Desktop\pp2\PP2\week2\Task2\input.txt");
-            string[] arr = input.ReadLine().Split();
+            return IsPrime(int.Parse(s));
+        }
 
-            StreamWriter output = new StreamWriter(@"C: \Users\Аружан\Desktop\pp2\PP2\week2\Task2\output.txt");
-            int[] arr2 = new int[100];
 
-            for(int i = 0; i < arr.Length; i++)
+
+        static void Main(string[] args)
+        {
+            List<string> primes = new List<string>();
+
+            FileStream fs = new FileStream(@"C:\Users\Аружан\Desktop\pp2\week2\Task2\input.txt", FileMode.Open, FileAccess.Read);
+            StreamReader input = new StreamReader(fs);
+
+            string line = input.ReadLine();
+            string[] arr = line.Split(' ');
+            
+            foreach (var x in arr)
             {
-                if(IsPrime(int.Parse(arr[i])) == true)
+                if (PrimeString(x))
                 {
-                    output.Write(int.Parse(arr[i]) + " ");
+                    primes.Add(x);
                 }
             }
 
             input.Close();
-            output.Close();
-        }
+            fs.Close();
 
-        static void Main(string[] args)
-        {
-            Primes();
+
+            FileStream fs2 = new FileStream(@"C: \Users\Аружан\Desktop\pp2\week2\Task2\output.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter output = new StreamWriter(fs2);
+
+            foreach(var x in primes)
+            {
+                output.Write(x + " ");
+                Console.Write(x + " ");
+            }
+
+            
+            output.Close();
+            fs2.Close();
         }
     }
 }
