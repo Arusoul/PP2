5aa4c6c3fd29e648a2c996e412f1bc6b0fca766a
Aruzhan Karazhanova <aruka010900@gmail.com>
2019-02-14 13:40:49 +0600

    task3 added and 1 and 2 modified

--- week2/Task3/Task3/Program.cs
+++ week2/Task3/Task3/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task3
 {
     class Program
     {
+        static void PrintSpace(int lvl) // spaces for every line before text
+        {
+            for(int i = 0; i < lvl; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void task(DirectoryInfo direct, int lvl)
+        {
+            FileInfo[] files = direct.GetFiles(); // an array with info about files
+            DirectoryInfo[] directories = direct.GetDirectories(); // // an array with xzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+            foreach(FileInfo file in files)
+            {
+                PrintSpace(lvl); // call the fuct with spaces for levels
+                Console.WriteLine(file.Name); // write names of the files to see them
+            }
+
+            foreach(DirectoryInfo d in directories)
+            {
+                PrintSpace(lvl);
+                Console.WriteLine(d.Name); // xzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz files of the files????????
+                task(d, lvl + 1); // xzzzzzzzzzzzzzzzzz
+            }
+        }
+
         static void Main(string[] args)
         {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Аружан\Desktop\pp2\week2");
+            task(d, 1);
+            Console.ReadKey();
         }
     }
 }
