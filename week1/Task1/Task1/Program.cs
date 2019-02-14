4d7c6d2f6d7dab2ed08f3489eaa1d8d73061bb26
Aruzhan Karazhanova <aruka010900@gmail.com>
2019-02-14 12:29:17 +0600

    edited

--- week1/Task1/Task1/Program.cs
+++ week1/Task1/Task1/Program.cs
@@ -8,50 +8,46 @@ namespace Task1
 {
     class Program
     {
+        public static bool Prime(int a) // create a function to find if the number is prime
+        {
+            bool ch = true;
+            if (a == 1) ch = false;
+            for(int i = 2; i < a; i++) // the checking cycle for every number is it divisible by numer -1
+            {
+                if(a % i == 0)
+                {
+                    ch = false;
+                } 
+            }
+            return ch; // return the result false or true
+        }
+
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine()); // to write an amount of an array elements using a string
-            int[] array = new int[n]; // declare an array with n elements
-            int[] primes = new int[n]; // declare an array which will be in the output
+
+            string s = Console.ReadLine();
+            string[] figures = s.Split(); // write each number of the array by the string and split them
 
             int cnt = 0; // the amount of primes in the output
 
-            string[] figures = Console.ReadLine().Split(); // write each number of the array by the string and split them
-
-            for (int i = 0; i < n; i++) 
+            for (int i = 0; i < n; i++)
             {
-                array[i] = int.Parse(figures[i]); // transform chars in the string array to figures with int.Parse() funct
+                int c = int.Parse(figures[i]); // transform an element of the array to figure
+                if (Prime(c) == true) cnt++; // checking numbers
             }
 
+        Console.WriteLine(cnt); // output of the amount of primes
             
-            for(int i = 0; i < n; i++)
+           for(int i = 0; i < n; i++)
             {
-                for (int j = 2; j < array[i]; j++) // compare every number until it's equal to an array element
+                int c = int.Parse(figures[i]);
+                if(Prime(c) == true)
                 {
-                    if(array[i] % j == 0) // if there is no remainder
-                    {
-                        continue; // ignore not prime nums
-                    }
-                    
+                    Console.WriteLine(c + " "); // output of primes with spaces
                 }
-
-                for(int i = 0; i < n; i++)
-                {
-                    if(array[i] > 1) // 1 isn't prime num, it starts from 2
-                    {
-                        primes[i] = array[i]; // give primes from the initial array to the empty new one
-                        cnt++; // count that primes
-                    }
-                }
-
-
-                Console.WriteLine(cnt); // output the amount of primes 
-                for (int j = 0; j < cnt; j++)
-                {
-                    Console.Write(primes[j] + " "); // output the new array
-                }
-                
+            }
+                      
             }
         }
-    }
-}
+ }
