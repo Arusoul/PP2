97fb28cb1e4cacd1279be8c4666d9355f9eca5ab
Aruzhan Karazhanova <aruka010900@gmail.com>
2019-04-25 18:45:03 +0600

    from pizza

---
+++ week9/G4 quiz/G4 quiz/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace G4_quiz
+{
+    class Ball
+
+    {
+
+        public Point Location { get; set; }
+
+        public Color Color { get; set; }
+
+        public bool Selected { get; set; }
+        public bool Deleted { get; set; }
+
+
+        public Ball(Point location, Color color)
+
+        {
+
+            this.Location = location;
+
+            this.Color = color;
+
+            Selected = false;
+            Deleted = false;
+        }
+
+
+
+    }
+}
---
+++ week9/G4 quiz/G4 quiz/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+using System.Data;
+
+using System.Drawing;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading;
+
+using System.Windows.Forms;
+
+
+
+namespace G4_quiz
+
+{
+
+    public partial class Form1 : Form
+
+    {
+
+        Bitmap bitmap;
+
+        Graphics graphics;
+
+
+
+        List<Ball> balls = new List<Ball>
+
+            {
+
+                new Ball(new Point(35, 100), Color.Green),
+
+                new Ball(new Point(123, 305), Color.Green),
+
+                new Ball(new Point(564, 341), Color.Purple),
+
+                new Ball(new Point(100, 200), Color.Yellow),
+
+                new Ball(new Point(231, 300), Color.Yellow),
+
+                new Ball(new Point(35,300), Color.CornflowerBlue),
+
+                new Ball(new Point(135, 234), Color.CornflowerBlue),
+
+                new Ball(new Point(123, 30), Color.Red),
+
+                new Ball(new Point(335, 45), Color.Purple),
+
+                new Ball(new Point(43, 43), Color.Red),
+
+
+
+            };
+
+
+
+        // Random rand = new Random(DateTime.Now.Millisecond);
+
+
+
+        public Form1()
+
+        {
+
+
+
+            InitializeComponent();
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            graphics = Graphics.FromImage(bitmap);
+
+            pictureBox1.Image = bitmap;
+            /*
+            ThreadStart ts = new ThreadStart(CheckBalls);
+
+            Thread th = new Thread(ts);
+
+            th.Start();
+            */
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        void CheckBalls()
+
+        {
+
+            foreach (Ball b in balls)
+
+            {
+
+                foreach (Ball b1 in balls)
+
+                {
+                    
+                    if (b.Color == b1.Color && b1.Selected && b.Selected && b != b1)
+                    {
+                        b.Deleted = true;
+
+                        b1.Deleted = true;
+                    }
+                }
+
+            }
+
+        }
+
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+
+        {
+
+            CheckBalls();
+
+
+
+            foreach (Ball b in balls)
+
+            {
+
+                if (!b.Deleted)
+
+                {
+
+                    e.Graphics.FillEllipse(new SolidBrush(b.Color), b.Location.X - 25, b.Location.Y - 25, 50, 50);
+
+                    if (b.Selected)
+
+                        e.Graphics.DrawEllipse(new Pen(Color.Black, 5), b.Location.X - 25, b.Location.Y - 25, 50, 50);
+
+                }
+
+            }
+
+
+
+
+
+        }
+
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+
+        {
+
+            foreach (Ball b in balls)
+
+            {
+
+                if (Math.Pow(e.X - (b.Location.X), 2) + Math.Pow(e.Y - (b.Location.Y), 2) <= 25 * 25)
+
+                {
+
+                    b.Selected = !b.Selected;
+
+                    Refresh();
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
